f6deb69f29a84e82ba66a80ad1452de60522ef87
POSTMD <info@thebestofosaka.com>
2019-02-12 11:13:02 +0900

    Initial Commit

---
+++ HitList2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HitList2
+//
+//  Created by POST MD on 2/12/19.
+//  Copyright © 2019 Grinning Zen Media and Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
